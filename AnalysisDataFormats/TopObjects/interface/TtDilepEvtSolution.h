jniedzie/cmssw

commit 065ecddd40bbd619b1756736ba4587c4589cef50

added resolution methods with more granularity

AnalysisDataFormats/TopObjects/interface/TtDilepEvtSolution.h
@@ -1,5 +1,5 @@
 //
-// $Id: TtDilepEvtSolution.h,v 1.17 2008/01/25 13:32:14 vadler Exp $
+// $Id: TtDilepEvtSolution.h,v 1.18 2008/02/15 12:10:47 rwolf Exp $
 //
 
 #ifndef TopObjects_TtDilepEvtSolution_h
@@ -80,7 +80,9 @@ class TtDilepEvtSolution {
   //-------------------------------------------
   // miscellaneous
   //-------------------------------------------
-  double getResidual()     const;
+  double getJetResidual()    const;
+  double getLeptonResidual() const;
+  double getFullResidual()   const { return getJetResidual()+getFullResidual(); }
   bool   getBestSol()      const { return bestSol_; }
   double getRecTopMass()   const {return topmass_; }
   double getRecWeightMax() const {return weightmax_; }

AnalysisDataFormats/TopObjects/src/TtDilepEvtSolution.cc
@@ -1,5 +1,5 @@
 //
-// $Id: TtDilepEvtSolution.cc,v 1.15 2008/01/25 13:34:29 vadler Exp $
+// $Id: TtDilepEvtSolution.cc,v 1.16 2008/02/15 12:10:53 rwolf Exp $
 //
 
 #include "PhysicsTools/Utilities/interface/DeltaR.h"
@@ -76,12 +76,31 @@ reco::Particle TtDilepEvtSolution::getLeptPos() const
 //-------------------------------------------
 // miscellaneous
 //-------------------------------------------
-double TtDilepEvtSolution::getResidual() const
+double TtDilepEvtSolution::getJetResidual() const
 {
   double distance = 0.;
   if(!getGenB() || !getGenBbar()) return distance;
   distance += DeltaR<reco::Particle,reco::GenParticle>()(getCalJetB(),*getGenB());
   distance += DeltaR<reco::Particle,reco::GenParticle>()(getCalJetBbar(),*getGenBbar());
+  return distance;
+}
+
+double TtDilepEvtSolution::getLeptonResidual() const
+{
+  double distance = 0.;
+  if(!getGenLepp() || !getGenLepm()) return distance;
+  if(getWpDecay()=="electron")
+    distance += DeltaR<reco::Particle,reco::GenParticle>()(getElectronp(),*getGenLepp());
+  else if(getWpDecay()=="muon")
+    distance += DeltaR<reco::Particle,reco::GenParticle>()(getMuonp(),*getGenLepp());
+  else if(getWpDecay()=="tau")
+    distance += DeltaR<reco::Particle,reco::GenParticle>()(getTaup(),*getGenLepp());
+  if(getWmDecay()=="electron")
+    distance += DeltaR<reco::Particle,reco::GenParticle>()(getElectronm(),*getGenLepm());
+  else if(getWmDecay()=="muon")
+    distance += DeltaR<reco::Particle,reco::GenParticle>()(getMuonm(),*getGenLepm());
+  else if(getWmDecay()=="tau")
+    distance += DeltaR<reco::Particle,reco::GenParticle>()(getTaum(),*getGenLepm());
   return distance;
 }
 

commit 2ad8f6d542764582e0202993885dbec72c259dc8

AnalysisDataFormats/TopObjects: Fix bug found by clang warning:
AnalysisDataFormats/TopObjects/interface/TtDilepEvtSolution.h:84:36: warning: all paths through this function will call itself [-Winfinite-recursion]
   double getFullResidual()   const { return getJetResidual()+getFullResidual(); }
                                   ^

I believe the bug was introduced in commit 065ecddd40bbd619b1756736ba4587c4589cef50

-  double getResidual()     const;
 +  double getJetResidual()    const;
 +  double getLeptonResidual() const;
 +  double getFullResidual()   const { return getJetResidual()+getFullResidual(); }

AnalysisDataFormats/TopObjects/interface/TtDilepEvtSolution.h
@@ -81,7 +81,7 @@ class TtDilepEvtSolution {
   //-------------------------------------------
   double getJetResidual()    const;
   double getLeptonResidual() const;
-  double getFullResidual()   const { return getJetResidual()+getFullResidual(); }
+  double getFullResidual()   const { return getJetResidual()+getLeptonResidual(); }
   bool   getBestSol()      const { return bestSol_; }
   double getRecTopMass()   const {return topmass_; }
   double getRecWeightMax() const {return weightmax_; }