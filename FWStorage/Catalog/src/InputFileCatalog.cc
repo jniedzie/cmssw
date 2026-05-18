Stux6-Technology/CMSSW-Development-project

Reduce the memory usage of the InputFileCatalog

FWStorage/Catalog/src/InputFileCatalog.cc
@@ -1,8 +1,12 @@
 #include <filesystem>
+#include <ranges>
+#include <utility>
 
 #include <boost/algorithm/string.hpp>
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Utilities/interface/Exception.h"
 #include "FWStorage/Catalog/interface/FileLocator.h"
@@ -10,12 +14,21 @@
 #include "FWStorage/Catalog/interface/SiteLocalConfig.h"
 
 namespace edm {
+  InputFileCatalog::InputFileCatalog(ParameterSet const& pset,
+                                     bool useLFNasPFNifLFNnotFound,
+                                     SciTagCategory sciTagCategory)
+      : InputFileCatalog(pset.getUntrackedParameter<std::vector<std::string> >("fileNames"),
+                         pset.getUntrackedParameter<std::string>("overrideCatalog", std::string()),
+                         useLFNasPFNifLFNnotFound,
+                         sciTagCategory) {}
 
-  InputFileCatalog::InputFileCatalog(std::vector<std::string> logicalFileNames,
+  InputFileCatalog::InputFileCatalog(std::vector<std::string> configuredFileNames,
                                      std::string const& override,
                                      bool useLFNasPFNifLFNnotFound,
                                      SciTagCategory sciTagCategory)
-      : sciTagCategory_(sciTagCategory) {
+      : configuredFileNames_(std::move(configuredFileNames)),
+        sciTagCategory_(sciTagCategory),
+        useLFNasPFNifLFNnotFound_(useLFNasPFNifLFNnotFound) {
     Service<SiteLocalConfig> localconfservice;
     if (!localconfservice.isAvailable()) {
       cms::Exception ex("FileCatalog");
@@ -24,7 +37,7 @@ namespace edm {
       throw ex;
     }
 
-    if (!overrideFileLocator_ && !override.empty()) {
+    if (!override.empty()) {
       //now make a struct from input string
       std::vector<std::string> tmps;
       boost::algorithm::split(tmps, override, boost::is_any_of(std::string(",")));
@@ -52,7 +65,6 @@ namespace edm {
       overrideFileLocator_ = std::make_unique<FileLocator>(override_struct, filename_storage);
     } else {
       std::vector<CatalogAttributes> const& tmp_dataCatalogs = localconfservice->dataCatalogs();
-      fileLocators_.clear();
       // Construct all file locators from data catalogs. If a data catalog is
       // invalid (wrong protocol for example), it is skipped and no file locator
       // is constructed (an exception is thrown out from the FileLocator constructor).
@@ -79,77 +91,144 @@ namespace edm {
       }
     }
 
-    fileCatalogItems_.reserve(logicalFileNames.size());
-    for (auto& lfn : logicalFileNames) {
-      boost::trim(lfn);
-      std::vector<std::string> pfns;
-      if (lfn.empty()) {
+    if (!configuredFileNames_.empty() && sciTagCategory_ != SciTagCategory::Undefined) {
+      Service<StorageURLModifier> sciTagConfigService;
+      if (!sciTagConfigService.isAvailable()) {
+        cms::Exception ex("FileCatalog");
+        ex << "edm::SciTagConfig service is not available";
+        ex.addContext("Calling edm::InputFileCatalog constructor");
+        throw ex;
+      }
+    }
+
+    for (auto& configuredFileName : configuredFileNames_) {
+      boost::trim(configuredFileName);
+      if (configuredFileName.empty()) {
         cms::Exception ex("FileCatalog");
         ex << "An empty string specified in the fileNames parameter for input source";
         ex.addContext("Calling edm::InputFileCatalog constructor");
         throw ex;
       }
-      if (isPhysical(lfn)) {
-        if (lfn.back() == ':') {
+      if (notLogicalFileName(configuredFileName)) {
+        if (configuredFileName.back() == ':') {
           cms::Exception ex("FileCatalog");
           ex << "An empty physical file name specified in the fileNames parameter for input source";
           ex.addContext("Calling edm::InputFileCatalog constructor");
           throw ex;
         }
-        pfns.push_back(lfn);
-        // Clear the LFN.
-        lfn.clear();
-      } else {
-        findFile(lfn, pfns, useLFNasPFNifLFNnotFound);
       }
-      lfn.shrink_to_fit();  // try to release memory
-
-      if (sciTagCategory_ != SciTagCategory::Undefined) {
-        Service<StorageURLModifier> sciTagConfigService;
-        if (!sciTagConfigService.isAvailable()) {
-          cms::Exception ex("FileCatalog");
-          ex << "edm::SciTagConfig service is not available";
-          ex.addContext("Calling edm::InputFileCatalog constructor");
-          throw ex;
-        }
-
-        for (auto& pfn : pfns) {
-          sciTagConfigService->modify(sciTagCategory_, pfn);
-        }
-      }
-
-      fileCatalogItems_.emplace_back(std::move(pfns), std::move(lfn));
+      configuredFileName.shrink_to_fit();  // try to release memory
     }
   }
 
   InputFileCatalog::~InputFileCatalog() = default;
 
-  std::vector<std::string> InputFileCatalog::fileNames(unsigned iCatalog) const {
-    std::vector<std::string> tmp;
-    tmp.reserve(fileCatalogItems_.size());
-    for (auto const& item : fileCatalogItems_) {
-      tmp.push_back(item.fileName(iCatalog));
-    }
-    return tmp;
+  void InputFileCatalog::fillDescription(ParameterSetDescription& desc) {
+    desc.addUntracked<std::vector<std::string> >("fileNames")->setComment("Names of files to be processed.");
+    desc.addUntracked<std::string>("overrideCatalog", std::string());
   }
 
-  void InputFileCatalog::findFile(std::string const& lfn,
-                                  std::vector<std::string>& pfns,
-                                  bool useLFNasPFNifLFNnotFound) const {
-    if (overrideFileLocator_) {
-      pfns.emplace_back(overrideFileLocator_->pfn(lfn));
+  bool InputFileCatalog::notLogicalFileName(std::string const& configuredFileName) {
+    return configuredFileName.find(':') != std::string::npos;
+  }
+
+  std::string const& InputFileCatalog::logicalFileName(unsigned int fileIndex) const {
+    if (fileIndex >= configuredFileNames_.size()) {
+      cms::Exception ex("FileCatalog");
+      ex << "Out of range argument";
+      ex.addContext("Calling edm::InputFileCatalog::logicalFileName with fileIndex");
+      throw ex;
+    }
+    return logicalFileName(configuredFileNames_[fileIndex]);
+  }
+
+  std::string const& InputFileCatalog::logicalFileName(std::string const& configuredFileName) const {
+    if (notLogicalFileName(configuredFileName)) {
+      return emptyString_;
     } else {
-      for (auto const& locator : fileLocators_) {
-        std::string pfn = locator->pfn(lfn);
-        if (pfn.empty() && useLFNasPFNifLFNnotFound) {
-          pfns.emplace_back(lfn);
-        } else {
-          pfns.emplace_back(std::move(pfn));
-        }
-      }
+      return configuredFileName;
+    }
+  }
+
+  std::vector<std::string> InputFileCatalog::physicalFileNames(std::string const& configuredFileName) const {
+    std::vector<std::string> pfns;
+
+    bool notLFN = notLogicalFileName(configuredFileName);
+    unsigned int numberOfPFNs = (notLFN || overrideFileLocator_) ? 1 : fileLocators_.size();
+    for (unsigned int iCatalog : std::views::iota(0u, numberOfPFNs)) {
+      pfns.push_back(physicalFileName(iCatalog, configuredFileName, notLFN));
+    }
+    return pfns;
+  }
+
+  std::vector<std::string> InputFileCatalog::physicalFileNames(unsigned int fileIndex) const {
+    if (fileIndex >= configuredFileNames_.size()) {
+      cms::Exception ex("FileCatalog");
+      ex << "Out of range argument";
+      ex.addContext("Calling edm::InputFileCatalog::physicalFileNames with fileIndex");
+      throw ex;
+    }
+    return physicalFileNames(configuredFileNames_[fileIndex]);
+  }
+
+  std::string InputFileCatalog::firstPFNFromFirstCatalog() const {
+    const unsigned int iCatalog = 0;
+    if (configuredFileNames_.empty()) {
+      // If using this function, there should have been at least
+      // one configured file name passed in to the InputFileCatalog
+      // constructor. The client of InputFileCatalog must ensure that
+      // before using this function.
+      cms::Exception ex("FileCatalog");
+      ex << "No file names configured";
+      ex.addContext("Calling edm::InputFileCatalog::firstPFNFromFirstCatalog");
+      throw ex;
+    }
+    std::string const& firstConfiguredFileName = configuredFileNames_[0];
+    bool notLFN = notLogicalFileName(firstConfiguredFileName);
+    return physicalFileName(iCatalog, firstConfiguredFileName, notLFN);
+  }
+
+  std::vector<std::string> InputFileCatalog::allPFNsFromFirstCatalog() const {
+    const unsigned int iCatalog = 0;
+    std::vector<std::string> pfns;
+    for (auto const& configuredFileName : configuredFileNames_) {
+      bool notLFN = notLogicalFileName(configuredFileName);
+      pfns.push_back(physicalFileName(iCatalog, configuredFileName, notLFN));
+    }
+    return pfns;
+  }
+
+  std::string InputFileCatalog::physicalFileName(unsigned int iCatalog,
+                                                 std::string const& configuredFileName,
+                                                 bool notLFN) const {
+    std::string pfn;
+
+    if (notLFN) {
+      pfn = configuredFileName;
+    } else {
+      pfn = pfnFromCatalog(iCatalog, configuredFileName);
     }
 
-    // Empty PFN will be found by caller.
+    // Append CGI parameters (sciTags) to complete the PFN
+    if (sciTagCategory_ != SciTagCategory::Undefined) {
+      Service<StorageURLModifier> sciTagConfigService;
+      sciTagConfigService->modify(sciTagCategory_, pfn);
+    }
+
+    return pfn;
+  }
+
+  std::string InputFileCatalog::pfnFromCatalog(unsigned int iCatalog, std::string const& lfn) const {
+    std::string pfn;
+    if (overrideFileLocator_) {
+      pfn = overrideFileLocator_->pfn(lfn);
+    } else {
+      pfn = fileLocators_[iCatalog]->pfn(lfn);
+      if (pfn.empty() && useLFNasPFNifLFNnotFound_) {
+        pfn = lfn;
+      }
+    }
+    return pfn;
   }
 
 }  // namespace edm

jniedzie/cmssw

FWStorage/Catalog/src/InputFileCatalog.cc
@@ -1,21 +1,38 @@
 #include <filesystem>
+#include <ranges>
+#include <utility>
 
 #include <boost/algorithm/string.hpp>
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Utilities/interface/Exception.h"
 #include "FWStorage/Catalog/interface/FileLocator.h"
 #include "FWStorage/Catalog/interface/InputFileCatalog.h"
 #include "FWStorage/Catalog/interface/SiteLocalConfig.h"
 
-namespace edm {
+namespace {
+  std::string const emptyString;
+}
 
-  InputFileCatalog::InputFileCatalog(std::vector<std::string> logicalFileNames,
+namespace edm {
+  InputFileCatalog::InputFileCatalog(ParameterSet const& pset,
+                                     bool useLFNasPFNifLFNnotFound,
+                                     SciTagCategory sciTagCategory)
+      : InputFileCatalog(pset.getUntrackedParameter<std::vector<std::string> >("fileNames"),
+                         pset.getUntrackedParameter<std::string>("overrideCatalog", std::string()),
+                         useLFNasPFNifLFNnotFound,
+                         sciTagCategory) {}
+
+  InputFileCatalog::InputFileCatalog(std::vector<std::string> configuredFileNames,
                                      std::string const& override,
                                      bool useLFNasPFNifLFNnotFound,
                                      SciTagCategory sciTagCategory)
-      : sciTagCategory_(sciTagCategory) {
+      : configuredFileNames_(std::move(configuredFileNames)),
+        sciTagCategory_(sciTagCategory),
+        useLFNasPFNifLFNnotFound_(useLFNasPFNifLFNnotFound) {
     Service<SiteLocalConfig> localconfservice;
     if (!localconfservice.isAvailable()) {
       cms::Exception ex("FileCatalog");
@@ -24,7 +41,7 @@ namespace edm {
       throw ex;
     }
 
-    if (!overrideFileLocator_ && !override.empty()) {
+    if (!override.empty()) {
       //now make a struct from input string
       std::vector<std::string> tmps;
       boost::algorithm::split(tmps, override, boost::is_any_of(std::string(",")));
@@ -52,7 +69,6 @@ namespace edm {
       overrideFileLocator_ = std::make_unique<FileLocator>(override_struct, filename_storage);
     } else {
       std::vector<CatalogAttributes> const& tmp_dataCatalogs = localconfservice->dataCatalogs();
-      fileLocators_.clear();
       // Construct all file locators from data catalogs. If a data catalog is
       // invalid (wrong protocol for example), it is skipped and no file locator
       // is constructed (an exception is thrown out from the FileLocator constructor).
@@ -79,77 +95,145 @@ namespace edm {
       }
     }
 
-    fileCatalogItems_.reserve(logicalFileNames.size());
-    for (auto& lfn : logicalFileNames) {
-      boost::trim(lfn);
-      std::vector<std::string> pfns;
-      if (lfn.empty()) {
+    if (!configuredFileNames_.empty() && sciTagCategory_ != SciTagCategory::Undefined) {
+      Service<StorageURLModifier> sciTagConfigService;
+      if (!sciTagConfigService.isAvailable()) {
+        cms::Exception ex("FileCatalog");
+        ex << "edm::SciTagConfig service is not available";
+        ex.addContext("Calling edm::InputFileCatalog constructor");
+        throw ex;
+      }
+    }
+
+    for (auto& configuredFileName : configuredFileNames_) {
+      boost::trim(configuredFileName);
+      if (configuredFileName.empty()) {
         cms::Exception ex("FileCatalog");
         ex << "An empty string specified in the fileNames parameter for input source";
         ex.addContext("Calling edm::InputFileCatalog constructor");
         throw ex;
       }
-      if (isPhysical(lfn)) {
-        if (lfn.back() == ':') {
+      if (isPhysicalFileName(configuredFileName)) {
+        if (configuredFileName.back() == ':') {
           cms::Exception ex("FileCatalog");
           ex << "An empty physical file name specified in the fileNames parameter for input source";
           ex.addContext("Calling edm::InputFileCatalog constructor");
           throw ex;
         }
-        pfns.push_back(lfn);
-        // Clear the LFN.
-        lfn.clear();
-      } else {
-        findFile(lfn, pfns, useLFNasPFNifLFNnotFound);
       }
-      lfn.shrink_to_fit();  // try to release memory
-
-      if (sciTagCategory_ != SciTagCategory::Undefined) {
-        Service<StorageURLModifier> sciTagConfigService;
-        if (!sciTagConfigService.isAvailable()) {
-          cms::Exception ex("FileCatalog");
-          ex << "edm::SciTagConfig service is not available";
-          ex.addContext("Calling edm::InputFileCatalog constructor");
-          throw ex;
-        }
-
-        for (auto& pfn : pfns) {
-          sciTagConfigService->modify(sciTagCategory_, pfn);
-        }
-      }
-
-      fileCatalogItems_.emplace_back(std::move(pfns), std::move(lfn));
+      configuredFileName.shrink_to_fit();  // try to release memory
     }
   }
 
   InputFileCatalog::~InputFileCatalog() = default;
 
-  std::vector<std::string> InputFileCatalog::fileNames(unsigned iCatalog) const {
-    std::vector<std::string> tmp;
-    tmp.reserve(fileCatalogItems_.size());
-    for (auto const& item : fileCatalogItems_) {
-      tmp.push_back(item.fileName(iCatalog));
-    }
-    return tmp;
+  void InputFileCatalog::fillDescription(ParameterSetDescription& desc) {
+    desc.addUntracked<std::vector<std::string> >("fileNames")->setComment("Names of files to be processed.");
+    desc.addUntracked<std::string>("overrideCatalog", std::string());
   }
 
-  void InputFileCatalog::findFile(std::string const& lfn,
-                                  std::vector<std::string>& pfns,
-                                  bool useLFNasPFNifLFNnotFound) const {
-    if (overrideFileLocator_) {
-      pfns.emplace_back(overrideFileLocator_->pfn(lfn));
+  bool InputFileCatalog::isPhysicalFileName(std::string const& configuredFileName) {
+    return configuredFileName.find(':') != std::string::npos;
+  }
+
+  std::string const& InputFileCatalog::logicalFileName(unsigned int fileIndex) const {
+    if (fileIndex >= configuredFileNames_.size()) {
+      cms::Exception ex("FileCatalog");
+      ex << "Out of range argument";
+      ex.addContext("Calling edm::InputFileCatalog::logicalFileName with fileIndex");
+      throw ex;
+    }
+    return logicalFileName(configuredFileNames_[fileIndex]);
+  }
+
+  std::string const& InputFileCatalog::logicalFileName(std::string const& configuredFileName) const {
+    if (isPhysicalFileName(configuredFileName)) {
+      return emptyString;
     } else {
-      for (auto const& locator : fileLocators_) {
-        std::string pfn = locator->pfn(lfn);
-        if (pfn.empty() && useLFNasPFNifLFNnotFound) {
-          pfns.emplace_back(lfn);
-        } else {
-          pfns.emplace_back(std::move(pfn));
-        }
-      }
+      return configuredFileName;
+    }
+  }
+
+  std::vector<std::string> InputFileCatalog::physicalFileNames(std::string const& configuredFileName) const {
+    std::vector<std::string> pfns;
+
+    bool isPFN = isPhysicalFileName(configuredFileName);
+    unsigned int numberOfPFNs = (isPFN || overrideFileLocator_) ? 1 : fileLocators_.size();
+    pfns.reserve(numberOfPFNs);
+    for (unsigned int iCatalog : std::views::iota(0u, numberOfPFNs)) {
+      pfns.push_back(physicalFileName(iCatalog, configuredFileName, isPFN));
+    }
+    return pfns;
+  }
+
+  std::vector<std::string> InputFileCatalog::physicalFileNames(unsigned int fileIndex) const {
+    if (fileIndex >= configuredFileNames_.size()) {
+      cms::Exception ex("FileCatalog");
+      ex << "Out of range argument";
+      ex.addContext("Calling edm::InputFileCatalog::physicalFileNames with fileIndex");
+      throw ex;
+    }
+    return physicalFileNames(configuredFileNames_[fileIndex]);
+  }
+
+  std::string InputFileCatalog::firstPFNFromFirstCatalog() const {
+    const unsigned int iCatalog = 0;
+    if (configuredFileNames_.empty()) {
+      // If using this function, there should have been at least
+      // one configured file name passed in to the InputFileCatalog
+      // constructor. The client of InputFileCatalog must ensure that
+      // before using this function.
+      cms::Exception ex("FileCatalog");
+      ex << "No file names configured";
+      ex.addContext("Calling edm::InputFileCatalog::firstPFNFromFirstCatalog");
+      throw ex;
+    }
+    std::string const& firstConfiguredFileName = configuredFileNames_[0];
+    bool isPFN = isPhysicalFileName(firstConfiguredFileName);
+    return physicalFileName(iCatalog, firstConfiguredFileName, isPFN);
+  }
+
+  std::vector<std::string> InputFileCatalog::allPFNsFromFirstCatalog() const {
+    const unsigned int iCatalog = 0;
+    std::vector<std::string> pfns;
+    for (auto const& configuredFileName : configuredFileNames_) {
+      bool isPFN = isPhysicalFileName(configuredFileName);
+      pfns.push_back(physicalFileName(iCatalog, configuredFileName, isPFN));
+    }
+    return pfns;
+  }
+
+  std::string InputFileCatalog::physicalFileName(unsigned int iCatalog,
+                                                 std::string const& configuredFileName,
+                                                 bool isPFN) const {
+    std::string pfn;
+
+    if (isPFN) {
+      pfn = configuredFileName;
+    } else {
+      pfn = pfnFromCatalog(iCatalog, configuredFileName);
     }
 
-    // Empty PFN will be found by caller.
+    // Append CGI parameters (sciTags) to complete the PFN
+    if (sciTagCategory_ != SciTagCategory::Undefined) {
+      Service<StorageURLModifier> sciTagConfigService;
+      sciTagConfigService->modify(sciTagCategory_, pfn);
+    }
+
+    return pfn;
+  }
+
+  std::string InputFileCatalog::pfnFromCatalog(unsigned int iCatalog, std::string const& lfn) const {
+    std::string pfn;
+    if (overrideFileLocator_) {
+      pfn = overrideFileLocator_->pfn(lfn);
+    } else {
+      pfn = fileLocators_[iCatalog]->pfn(lfn);
+      if (pfn.empty() && useLFNasPFNifLFNnotFound_) {
+        pfn = lfn;
+      }
+    }
+    return pfn;
   }
 
 }  // namespace edm