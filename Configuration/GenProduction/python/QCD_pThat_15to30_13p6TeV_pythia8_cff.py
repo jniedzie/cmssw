2c6966927cd8b63a87469e387f29bb3a723d9df2
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2026-07-23 16:31:48 +0200

    Adding pythia fragment for J/Ψ production + updating gitignore.

---
+++ Configuration/GenProduction/python/QCD_pThat_15to30_13p6TeV_pythia8_cff.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import pythia8CommonSettingsBlock
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import pythia8CP5SettingsBlock
+
+generator = cms.EDFilter(
+    "Pythia8GeneratorFilter",
+    pythiaPylistVerbosity=cms.untracked.int32(0),
+    filterEfficiency=cms.untracked.double(1.0),
+    pythiaHepMCVerbosity=cms.untracked.bool(False),
+    comEnergy=cms.double(13600.0),
+    maxEventsToPrint=cms.untracked.int32(1),
+    PythiaParameters=cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters=cms.vstring(
+            'Charmonium:all = on',       # prompt charmonium production (J/psi, chi_c, psi(2S), ...)
+            'PhaseSpace:pTHatMin = 1.',  # low pT bin, since you want low-pT J/psi
+            'PhaseSpace:pTHatMax = 5.',
+        ),
+        parameterSets=cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CP5Settings',
+            'processParameters',
+        )
+    )
+)
