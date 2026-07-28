bb1d144f543cbceb3456468c2f000dc8934930dd
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2026-07-28 15:46:48 +0200

    Fixing initial issues with muon segments setup

--- PhysicsTools/ShiftMuonSegments/plugins/ShiftMuonSegmentsCounter.cc
+++ PhysicsTools/ShiftMuonSegments/plugins/ShiftMuonSegmentsCounter.cc
@@ -1,4 +1,5 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
--- PhysicsTools/ShiftMuonSegments/python/test_shiftMuonSegments_cfg.py
+++ PhysicsTools/ShiftMuonSegments/python/test_shiftMuonSegments_cfg.py
@@ -1,6 +1,17 @@
 import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
 from PhysicsTools.ShiftMuonSegments.shiftMuonSegments_cfi import shiftMuonSegments, shiftMuonSegmentsCounter
+
+options = VarParsing("analysis")
+options.register("inputFile", "file:events_AOD.root", VarParsing.multiplicity.singleton, VarParsing.varType.string, "AOD input file")
+options.outputFile = "shiftMuonSegments_test.root"
+options.parseArguments()
+
 process = cms.Process("SHIFTSEGMENTS")
-process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(10))
-process.source = cms.Source("PoolSource", fileNames=cms.untracked.vstring("file:events_AOD.root"))
-process.p = cms.Path(shiftMuonSegmentsCounter + shiftMuonSegments)
+process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(options.maxEvents))
+process.source = cms.Source("PoolSource", fileNames=cms.untracked.vstring(options.inputFile))
+process.shiftMuonSegmentsCounter = shiftMuonSegmentsCounter.clone()
+process.shiftMuonSegments = shiftMuonSegments.clone()
+process.p = cms.Path(process.shiftMuonSegmentsCounter + process.shiftMuonSegments)
+process.out = cms.OutputModule("PoolOutputModule", fileName=cms.untracked.string(options.outputFile), SelectEvents=cms.untracked.PSet(SelectEvents=cms.vstring("p")))
+process.end = cms.EndPath(process.out)
