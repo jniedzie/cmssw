e9ca57268d35a9cb2412f70010378669ad85ce37
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2026-07-30 16:33:46 +0200

    Adding missing files from ShiftMuonSegments module.

--- PhysicsTools/ShiftMuonSegments/python/shiftMuonSegments_cfi.py
+++ PhysicsTools/ShiftMuonSegments/python/shiftMuonSegments_cfi.py
@@ -1,4 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-shiftMuonSegments = cms.EDProducer("ShiftMuonSegmentsTableProducer", dtSegments=cms.InputTag("dt4DSegments"), cscSegments=cms.InputTag("cscSegments"))
-shiftMuonSegmentsCounter = cms.EDAnalyzer("ShiftMuonSegmentsCounter", dtSegments=shiftMuonSegments.dtSegments, cscSegments=shiftMuonSegments.cscSegments)
+shiftMuonSegments = cms.EDProducer(
+    "ShiftMuonSegmentsTableProducer",
+    dtSegments=cms.InputTag("dt4DSegments"),
+    cscSegments=cms.InputTag("cscSegments"),
+)
+shiftMuonSegmentsCounter = cms.EDAnalyzer(
+    "ShiftMuonSegmentsCounter",
+    dtSegments=shiftMuonSegments.dtSegments,
+    cscSegments=shiftMuonSegments.cscSegments,
+)
